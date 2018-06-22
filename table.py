ced3b10c8a36d8549a8971e073219eaf4ae3c618
lahiru94 <lahirud.14@cse.mrt.ac.lk>
2018-06-22 10:30:04 +0530

    basic visualization

--- table.py
+++ table.py
@@ -5,45 +5,50 @@ import pandas as pd
 class TableEntity(object):
 
     def __init__(self):
-        self.labels = []
+        self.predicted_labels = []
+        self.true_label = None
 
     def add_label(self, label):
-        self.labels.append(label)
+        self.predicted_labels.append(label)
 
     def get_labels(self):
-        return self.labels
+        return self.predicted_labels
 
 
 class Table(TableEntity):
 
     def __init__(self):
         self.columns = []
+        self.rows = []
 
     def get_NE_cols(self):
-        cols = []
+        col_indices = []
+        i = 0
         for column in self.columns:
             if(column.is_NE()):
-                cols.append(column)
-        sub_table = Table()
-        sub_table.columns = cols
+                col_indices.append(i)
+            i += 1
+        sub_table = self.get_subtable(col_indices, "col")
         return sub_table
-
+    
     def get_numeric_cols(self):
-        cols = []
+        col_indices = []
+        i = 0
         for column in self.columns:
             if(column.is_numeric()):
-                cols.append(column)
-        sub_table = Table()
-        sub_table.columns = cols
+                col_indices.append(i)
+            i += 1
+        sub_table = self.get_subtable(col_indices, "col")
         return sub_table
 
     def get_date_cols(self):
-        cols = []
+        col_indices = []
+        i = 0
         for column in self.columns:
             if(column.is_date()):
-                cols.append(column)
-        sub_table = Table()
-        sub_table.columns = cols
+                col_indices.append(i)
+            i += 1
+        sub_table = self.get_subtable(col_indices, "col")
         return sub_table
 
     def parse_csv(self, file_path):
@@ -55,11 +60,55 @@ class Table(TableEntity):
             for value in values:
                 cells.append(Cell(value[0]))
             self.columns.append(Column(header, cells))
+        row_list = temp_df.values.tolist()
+        for row in row_list:
+            cells = []
+            for value in row:
+                cells.append(Cell(value))
+            self.rows.append(Row(cells))
+
+    def get_subtable(self, indices=[], axis="col"):
+        new_cols = []
+        new_rows = []
+        indices.sort()
+        if(axis == "col"):
+            for i in indices:
+                new_cols.append(self.columns[i])
+            for row in self.rows:
+                row_cells = []
+                for j in indices:
+                    row_cells.append(row.cells[j])
+                new_rows.append(Row(row_cells))
+        sub_table = Table()
+        sub_table.columns = new_cols
+        sub_table.rows = new_rows
+        return sub_table
+
+    def visualize(self):
+        html_head = "<!DOCTYPE html><html><head><meta charset=\"UTF-8\"><title>title</title></head><body><table><tr>"
+        html_tail = "</table></body></html>"
+
+        for col in self.columns:
+            th_title = "title='True Concept:{} &#xA;Predicted Concepts:".format(col.true_label)
+            for label in col.predicted_labels:
+                th_title = th_title+label
+            th_title = th_title + "'"
+            curr_header = "<th {}>{}</th>".format(th_title, col.header)
+            html_head = html_head + curr_header
+
+        html_head = html_head + "</tr>"
+
+        for row in self.rows:
+            html_head = html_head + row.visualize()
+        html = html_head+html_tail
+        with open("output.html", 'wb') as f:
+            f.write(html)        
 
 
 class Column(TableEntity):
 
     def __init__(self, header, cells):
+        super(Column, self).__init__()
         self.header = header
         self.cells = cells
         self.labels = []
@@ -107,9 +156,24 @@ class Column(TableEntity):
         return self.date_col
 
 
+class Row():
+
+    def __init__(self, cells):
+        # super(Row, self).__init__()
+        self.cells = cells
+
+    def visualize(self):
+        html = "<tr>"
+        for cell in self.cells:
+            html = html + cell.visualize()
+        html = html + "</tr>"
+        return html
+
+
 class Cell(TableEntity):
 
     def __init__(self, value):
+        super(Cell, self).__init__()
         self.value = value
 
     def is_NE(self):
@@ -136,10 +200,22 @@ class Cell(TableEntity):
         except TypeError:
             return False
 
+    def visualize(self):
+        html_actual = "<td title=\"Actual concept: {}".format(self.true_label)
+        html_predicted = "&#xA;Predicted concepts: "
+        html_value = "\">{}</td>".format(self.value)
+        for label in self.predicted_labels:
+            html_predicted = html_predicted + label
+        html = html_actual+html_predicted+html_value
+        return html
+
 
 if __name__ == "__main__":
     test_table = Table()
     test_table.parse_csv('sample.csv')
-    ne_table = test_table.get_numeric_cols()
-    for col in ne_table.columns:
-        print col.header
+    ne_table = test_table.get_NE_cols()
+    for row in ne_table.rows:
+        for cell in row.cells:
+            print cell.value,
+        print ""
+    ne_table.visualize()
